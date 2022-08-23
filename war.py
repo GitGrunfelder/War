1e0ea2ac6dc5d4484998e2b871d70d6f905d4048
George Grunfelder <68717396+GitGrunfelder@users.noreply.github.com>
2022-08-22 21:33:17 -0400

    Arranged Suits

--- war.py
+++ war.py
@@ -1,79 +1,11 @@
 from random import shuffle
 
-class Deck:
-    def __init__(self):
-        self.cards = []
-        for i in range(2,15):
-            for j in range(4):
-                self.cards.append(Card(i,j))
-        shuffle(self.cards)
-        
-    def rm_card(self):
-        if len(self.cards) == 0:
-            return
-        return self.cards.pop()
-    
-    
-class Player:
-    def __init__(self, name):
-        self.wins = 0
-        self.card = None
-        self.name = name
 
-
-class Game:
-    def __init__(self):
-        name1 = input("p1 name " )
-        name2 = input("p2 name ")
-        self.deck = Deck()
-        self.p1 = Player(name1)
-        self.p2 = Player(name2)
-        
-    def wins(self, winner):
-        w = f"{winner} wins this round"
-        print(w)
-        
-    def draw(self, p1n, p1c, p2n, p2c):
-        d = f"{p1n} drew {p1c} {p2n} drew {p2c}"
-        print(d)
-        
-    def play_game(self):
-        cards = self.deck.cards
-        print("beginning War!")
-        while len(cards) >= 2:
-            m = "q to quit. Any " + "key to play:"
-            response = input(m)
-            if response == "q":
-                break
-            p1c = self.deck.rm_card()
-            p2c = self.deck.rm_card()
-            p1n = self.p1.name
-            p2n = self.p2.name
-            self.draw(p1n,p1c,p2n,p2c)
-            if p1c > p2c:
-                self.p1.wins += 1
-                self.wins(self.p1.name)
-            else:
-                self.p2.wins += 1
-                self.wins(self.p2.name)
-        
-        win = self.winner(self.p1, self.p2)
-        
-        print(f"War is over. {win} wins!")
-        
-    def winner(self, p1, p2):
-        if p1.wins > p2.wins:
-            return p1.name
-        if p1.wins < p2.wins:
-            return p2.name
-        return "It was a tie!"
-        
-    
 class Card:
-    suits = ("spades",
+    suits = ("clubs",
              "hearts",
              "diamonds",
-             "clubs")
+             "spades")
     
     values = (None, None, "2", "3",
               "4", "5", "6", "7",
@@ -109,6 +41,78 @@ class Card:
         v = self.values[self.value] + " of " \
             + self.suits[self.suit]
         return v
+    
+    
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for i in range(2,15): # For each value in outer list,
+            for j in range(4): # Iterate through each suit
+                self.cards.append(Card(i,j)) # and append to list cards.
+        shuffle(self.cards) # Shuffle list in place.
+        
+    def rm_card(self):
+        if len(self.cards) == 0:
+            return
+        return self.cards.pop()
+    
+    
+class Player:
+    def __init__(self, name):
+        self.wins = 0
+        self.card = None
+        self.name = name
+
+
+class Game:
+    def __init__(self):
+        name1 = input("p1 name ")
+        name2 = input("p2 name ")
+        self.deck = Deck() # Creating a deck from the Deck function.
+        self.p1 = Player(name1) # Creating a player object from name1 input prompt.
+        self.p2 = Player(name2)
+        
+    def wins(self, winner):
+        w = f"{winner} wins this round"
+        print(w)
+        
+    def draw(self, p1n, p1c, p2n, p2c):
+        d = f"{p1n} drew {p1c} {p2n} drew {p2c}"
+        print(d)
+        
+    def play_game(self):
+        cards = self.deck.cards # This is assigning a shuffled deck to the cards variable.
+        print("beginning War!")
+        while len(cards) >= 2: # While at least 2 cards remain
+            m = "q to quit. Any " + "key to play:"
+            response = input(m)
+            if response == "q":
+                break
+            p1c = self.deck.rm_card() # Remove a card from the shuffled cards list, assign to p1
+            p2c = self.deck.rm_card()
+            p1n = self.p1.name # Assign p1 name to p1n
+            p2n = self.p2.name
+            self.draw(p1n,p1c,p2n,p2c)
+            if p1c > p2c:
+                self.p1.wins += 1
+                self.wins(self.p1.name)
+            else:
+                self.p2.wins += 1
+                self.wins(self.p2.name)
+        
+        win = self.winner(self.p1, self.p2)
+        
+        print(f"War is over. {win} wins!")
+        
+    def winner(self, p1, p2):
+        if p1.wins > p2.wins:
+            return p1.name
+        if p1.wins < p2.wins:
+            return p2.name
+        return "It was a tie!"
+        
+    
+
     
     
 game = Game()
